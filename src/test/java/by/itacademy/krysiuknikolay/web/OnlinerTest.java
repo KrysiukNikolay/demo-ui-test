481189d0d495145faca5b6aef4e77b567b9778c0
KrysiukNikolay <Kolian888@yandex.ru>
2023-02-13 10:29:02 +0300

    add new testOpenOnlinerLoginForm method

--- src/test/java/by/itacademy/krysiuknikolay/web/OnlinerTest.java
+++ src/test/java/by/itacademy/krysiuknikolay/web/OnlinerTest.java
@@ -4,12 +4,11 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OnlinerTest {
-@Test
-    public void testOpenOnliner(){
+    @Test
+    public void testOpenOnliner() {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(OnlinerPage.URL);
@@ -19,4 +18,14 @@ public class OnlinerTest {
         driver.quit();
     }
 
+    @Test
+    public void testOpenOnlinerLoginForm() {
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get(OnlinerPage.URL);
+    driver.findElement(By.xpath("//div[@class='auth-bar__item auth-bar__item--text']")).click();
+    Assert.assertEquals("Вход",driver.findElement(By.xpath("//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']")).getText());
+
+    driver.quit();
+    }
 }
