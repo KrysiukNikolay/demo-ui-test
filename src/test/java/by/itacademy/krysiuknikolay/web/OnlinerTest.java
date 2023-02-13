5e292d462a20044857ca0209a2945bb3512779d1
KrysiukNikolay <Kolian888@yandex.ru>
2023-02-13 10:21:57 +0300

    add new testOpenOnliner method

--- src/test/java/by/itacademy/krysiuknikolay/web/OnlinerTest.java
+++ src/test/java/by/itacademy/krysiuknikolay/web/OnlinerTest.java
@@ -1,4 +1,22 @@
 package by.itacademy.krysiuknikolay.web;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class OnlinerTest {
+@Test
+    public void testOpenOnliner(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnlinerPage.URL);
+        driver.findElements(By.xpath(OnlinerPage.COPYRIGHT));
+        Assert.assertEquals("© 2001—2023 Onlíner", driver.findElement(By.xpath(OnlinerPage.COPYRIGHT)).getText());
+
+        driver.quit();
+    }
+
 }
