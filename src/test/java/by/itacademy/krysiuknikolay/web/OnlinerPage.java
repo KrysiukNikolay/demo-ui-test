4625c96a2c491a350b3cbb73600c44eb59ad989e
KrysiukNikolay <Kolian888@yandex.ru>
2023-02-14 09:31:36 +0300

    add new testOnlinerLoginFormWithEmptyPassword method

--- src/test/java/by/itacademy/krysiuknikolay/web/OnlinerPage.java
+++ src/test/java/by/itacademy/krysiuknikolay/web/OnlinerPage.java
@@ -1,7 +1,13 @@
 package by.itacademy.krysiuknikolay.web;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class OnlinerPage {
+
     public static final String URL = "https://www.onliner.by/";
     public static final String COPYRIGHT = "//div[@class='footer-style__copy']";
+
 
 }
--- src/test/java/by/itacademy/krysiuknikolay/web/OnlinerTest.java
+++ src/test/java/by/itacademy/krysiuknikolay/web/OnlinerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class OnlinerTest {
     @Test
@@ -12,7 +13,6 @@ public class OnlinerTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(OnlinerPage.URL);
-        driver.findElements(By.xpath(OnlinerPage.COPYRIGHT));
         Assert.assertEquals("© 2001—2023 Onlíner", driver.findElement(By.xpath(OnlinerPage.COPYRIGHT)).getText());
 
         driver.quit();
@@ -29,14 +29,30 @@ public class OnlinerTest {
     driver.quit();
     }
     @Test
-    public void testOnlinerLoginFormWithEmptyCredentials () {
+    public void testOnlinerLoginFormWithEmptyCredentials() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(OnlinerPage.URL);
         driver.findElement(By.xpath("//div[@class='auth-bar__item auth-bar__item--text']")).click();
         driver.findElement(By.xpath("//div[@class='auth-form__control auth-form__control_condensed-additional']/button[@type='submit']")).click();
-        Assert.assertEquals("Укажите ник или e-mail",driver.findElement(By.xpath("//text()[contains(., 'Укажите ник или e-mail')]")).getText());
-        Assert.assertEquals("Укажите пароль",driver.findElement(By.xpath("//text()[contains(., 'Укажите пароль')]")).getText());
+        Thread.sleep(2000);
+        Assert.assertEquals("Укажите ник или e-mail",driver.findElement(By.xpath("//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div")).getText());
+        Assert.assertEquals("Укажите пароль",driver.findElement(By.xpath("//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div[2]/div")).getText());
+
+        driver.quit();
+    }
+    @Test
+    public void testOnlinerLoginFormWithEmptyPassword() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(OnlinerPage.URL);
+        driver.findElement(By.xpath("//div[@class='auth-bar__item auth-bar__item--text']")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Ник или e-mail']")).sendKeys("test@gmail.com");
+        driver.findElement(By.xpath("//div[@class='auth-form__control auth-form__control_condensed-additional']/button[@type='submit']")).click();
+        Thread.sleep(2000);
+        Assert.assertEquals("Укажите пароль", driver.findElement(By.xpath("//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div[2]/div")).getText());
+        Assert.assertTrue(driver.findElements(By.xpath("//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div")).size() == 0);
+
         driver.quit();
     }
 }
