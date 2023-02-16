68aa24f0495e57cdb1602a097cffb1b55276a38a
KrysiukNikolay <Kolian888@yandex.ru>
2023-02-16 13:40:49 +0300

    refactor OnlinerPage and OnlinerTest class

--- src/test/java/by/itacademy/krysiuknikolay/web/OnlinerPage.java
+++ src/test/java/by/itacademy/krysiuknikolay/web/OnlinerPage.java
@@ -3,12 +3,16 @@ package by.itacademy.krysiuknikolay.web;
 public class OnlinerPage {
 
     public static final String URL = "https://www.onliner.by/";
-    public static final String COPYRIGHT = "//div[@class='footer-style__copy']";
+    public static final String COPYRIGHT_XPATH = "//div[@class='footer-style__copy']";
+    public static final String COPYRIGHT_EXPECTED_TEXT = "© 2001—2023 Onlíner";
     public static final String BTN_AUTORIZATION_BASIC = "//div[@class='auth-bar__item auth-bar__item--text']";
     public static final String BTN_AUTORIZATION_NEW_PAGE = "//div[@class='auth-form__control auth-form__control_condensed-additional']/button[@type='submit']";
-    public static final String FORM_HEADER_TEXT = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
-    public static final String EMPTY_EMAIL_AND_LOGIN_ELEMENT = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
-    public static final String EMPTY_PASWORD_ELEMENT = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div[2]/div";
+    public static final String FORM_HEADER_XPATH = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
+    public static final String FORM_HEADER_EXPECTED_TEXT = "Вход";
+    public static final String EMPTY_EMAIL_ERROR_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
+    public static final String EMPTY_EMAIL_ERROR_EXPECTED_TEXT = "Укажите ник или e-mail";
+    public static final String EMPTY_PASWORD_ERROR_XPATH = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div[2]/div";
+    public static final String EMPTY_PASWORD_ERROR_EXPECTED_TEXT = "Укажите пароль";
 
     public static final String LOGIN_ENTRY_WINDOW = "//input[@placeholder='Ник или e-mail']";
 
--- src/test/java/by/itacademy/krysiuknikolay/web/OnlinerTest.java
+++ src/test/java/by/itacademy/krysiuknikolay/web/OnlinerTest.java
@@ -1,63 +1,65 @@
 package by.itacademy.krysiuknikolay.web;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class OnlinerTest {
+    WebDriver driver;
+
+    @Before
+    public void warmUP() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get(OnlinerPage.URL);
+    }
+
     @Test
     public void testOpenOnliner() {
         //Checking the display of the "copyright" element on the page
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(OnlinerPage.URL);
-        Assert.assertEquals("© 2001—2023 Onlíner", driver.findElement(By.xpath(OnlinerPage.COPYRIGHT)).getText());
+        String actualText = driver.findElement(By.xpath(OnlinerPage.COPYRIGHT_XPATH)).getText();
+        Assert.assertEquals(OnlinerPage.COPYRIGHT_EXPECTED_TEXT, actualText);
 
-        driver.quit();
     }
 
     @Test
     public void testOpenOnlinerLoginForm() {
         //Checking the display of an element "Вход" on the next page
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(OnlinerPage.URL);
         driver.findElement(By.xpath(OnlinerPage.BTN_AUTORIZATION_BASIC)).click();
-        Assert.assertEquals("Вход", driver.findElement(By.xpath(OnlinerPage.FORM_HEADER_TEXT)).getText());
-
-        driver.quit();
+        String actual = driver.findElement(By.xpath(OnlinerPage.FORM_HEADER_XPATH)).getText();
+        Assert.assertEquals(OnlinerPage.FORM_HEADER_EXPECTED_TEXT, actual);
     }
 
     @Test
-    public void testOnlinerLoginFormWithEmptyCredentials() throws InterruptedException {
+    public void testOnlinerLoginFormWithEmptyCredentials() {
         //Log in to the authorization page and enter empty values
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(OnlinerPage.URL);
         driver.findElement(By.xpath(OnlinerPage.BTN_AUTORIZATION_BASIC)).click();
         driver.findElement(By.xpath(OnlinerPage.BTN_AUTORIZATION_NEW_PAGE)).click();
-        Thread.sleep(2000);
-        Assert.assertEquals("Укажите ник или e-mail", driver.findElement(By.xpath(OnlinerPage.EMPTY_EMAIL_AND_LOGIN_ELEMENT)).getText());
-        Assert.assertEquals("Укажите пароль", driver.findElement(By.xpath(OnlinerPage.EMPTY_PASWORD_ELEMENT)).getText());
-
-        driver.quit();
+        String actualEmailErrorText = driver.findElement(By.xpath(OnlinerPage.EMPTY_EMAIL_ERROR_XPATH)).getText();
+        Assert.assertEquals(OnlinerPage.EMPTY_EMAIL_ERROR_EXPECTED_TEXT, actualEmailErrorText);
+        String actualPasswordErrorText = driver.findElement(By.xpath(OnlinerPage.EMPTY_PASWORD_ERROR_XPATH)).getText();
+        Assert.assertEquals(OnlinerPage.EMPTY_PASWORD_ERROR_EXPECTED_TEXT, actualPasswordErrorText);
     }
 
     @Test
-    public void testOnlinerLoginFormWithEmptyPassword() throws InterruptedException {
+    public void testOnlinerLoginFormWithEmptyPassword() {
         //Log in to the authorization page and enter a value in the email and an empty value in the password
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(OnlinerPage.URL);
         driver.findElement(By.xpath(OnlinerPage.BTN_AUTORIZATION_BASIC)).click();
         driver.findElement(By.xpath(OnlinerPage.LOGIN_ENTRY_WINDOW)).sendKeys("test@gmail.com");
         driver.findElement(By.xpath(OnlinerPage.BTN_AUTORIZATION_NEW_PAGE)).click();
-        Thread.sleep(2000);
-        Assert.assertEquals("Укажите пароль", driver.findElement(By.xpath(OnlinerPage.EMPTY_PASWORD_ELEMENT)).getText());
-        Assert.assertTrue(driver.findElements(By.xpath(OnlinerPage.EMPTY_EMAIL_AND_LOGIN_ELEMENT)).size() == 0);
+        String actualPasswordErrorText = driver.findElement(By.xpath(OnlinerPage.EMPTY_PASWORD_ERROR_XPATH)).getText();
+        Assert.assertEquals(OnlinerPage.EMPTY_PASWORD_ERROR_EXPECTED_TEXT,actualPasswordErrorText );
+    }
 
+    @After
+    public void after() {
         driver.quit();
     }
 }
